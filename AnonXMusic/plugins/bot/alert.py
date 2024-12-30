6f7a2bdeee53c6726484df4b12662396039818fa
saiboks <botssai69@gmail.com>
2024-12-30 13:55:33 +0530

    Update alert.py

--- AnonXMusic/plugins/bot/alert.py
+++ AnonXMusic/plugins/bot/alert.py
@@ -4,8 +4,8 @@ from pyrogram.types import Message
 from pyrogram import filters
 from AnonXMusic import app
 from config import BOT_USERNAME
-from AnonXMusic.mongo.readable_time import get_readable_time
-from AnonXMusic.mongo.afkdb import add_afk, is_afk, remove_afk
+from AnonXMusic.core.readable_time import get_readable_time
+from AnonXMusic.core.afkdb import add_afk, is_afk, remove_afk
 
 @app.on_message(filters.command(["afk", "brb"], prefixes=["/", "!",]))
 async def active_afk(_, message: Message):
