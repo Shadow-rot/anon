6868faf8810df594118d992d9a5c84b422fdf910
Shadow-rot <looktouhid@gmail.com>
2024-12-30 13:07:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -84,7 +84,7 @@ START_IMG_URL = getenv(
     "START_IMG_URL", "https://files.catbox.moe/x2ia0o.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/bsb294.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/zcxplk.mp4"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
