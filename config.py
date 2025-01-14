a91952d76824475b00f924e8f0dfedfbd1d6ecc9
Shadow-rot <looktouhid@gmail.com>
2025-01-14 22:17:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,10 +22,10 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600))
 
 # Chat id of a group for logging bot's activities
-LOGGER_ID = int(getenv("LOGGER_ID", None))
+LOGGER_ID = int(getenv("LOGGER_ID", -1002059929123))
 
 # LOGGER_ID Id Also Use No Problem
-BOTADDLOGS = int(getenv("BOTADDLOGS", "-1002218303560"))
+BOTADDLOGS = int(getenv("BOTADDLOGS", "-1002059929123"))
 
 # Get this value from @FallenxBot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 5147822244))
