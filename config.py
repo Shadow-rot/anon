5845aceaaf6122d6f0d86c42edefc9d5e9cbd522
Shadow-rot <looktouhid@gmail.com>
2025-02-01 09:50:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Shadow-rot/AnonXMusic",
+    "https://github.com/Shadow-rot/anon",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
