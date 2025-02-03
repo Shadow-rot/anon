4c52a288e0fedf616bfb5496360a9601702c8e60
Shadow-rot <looktouhid@gmail.com>
2025-02-03 11:10:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ BOT_USERNAME = getenv("BOT_USERNAME", "None")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 600000))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60000))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", -1002059929123))
