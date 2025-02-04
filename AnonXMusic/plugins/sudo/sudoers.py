78eb238aac4fc6c900d8b40a865c7f8828e0934a
saiboks <botssai69@gmail.com>
2025-02-04 20:24:51 +0530

    Update sudoers.py

--- AnonXMusic/plugins/sudo/sudoers.py
+++ AnonXMusic/plugins/sudo/sudoers.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from AnonXMusic import app
-from config import OWNER ID 
 from AnonXMusic.utils.database import add_sudo, remove_sudo
 from AnonXMusic.utils.decorators.language import language
+from AnonXMusic.misc import SUDOERS
 from AnonXMusic.utils.extraction import extract_user
 from AnonXMusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
