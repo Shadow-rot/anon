927c684193a4ba888c69aeff19a4f7a89ae02653
Shadow-rot <looktouhid@gmail.com>
2025-03-02 05:28:24 +0530

    Update utag.py

--- AnonXMusic/plugins/tools/utag.py
+++ AnonXMusic/plugins/tools/utag.py
@@ -23,7 +23,7 @@ async def tag_all_users(_,message):
             if message.chat.id not in spam_chats:
                 break       
             usernum += 5
-            usertxt += f"\n⊚ [{m.user.first_name}](.mention{m.user.id})\n"
+            usertxt += f"\n⊚ {user.mention}\n"
             if usernum == 1:
                 await replied.reply_text(usertxt)
                 await asyncio.sleep(3)
