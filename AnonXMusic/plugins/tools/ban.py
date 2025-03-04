e5b2084ed90a725c53a33eb2eabd09fbf8c1742b
Shadow-rot <looktouhid@gmail.com>
2025-03-04 06:00:46 +0530

    Update ban.py

--- AnonXMusic/plugins/tools/ban.py
+++ AnonXMusic/plugins/tools/ban.py
@@ -31,7 +31,7 @@ from config import BANNED_USERS
 warnsdb = mongodb.warns
 
 __MODULE__ = "Bᴀɴ"
-__HELP__ = """
+__HELP__ = """ """"
 /ban - Ban A User
 /banall - Ban All Users
 /sban - Delete all messages of user that sended in group and ban the user
@@ -56,7 +56,7 @@ __HELP__ = """
 /tmute - Mute A User For Specific Time
 /unmute - Unmute A User
 /zombies - Ban Deleted Accounts
-/report | @admins | @admin - Report A Message To Admins."""
+/report | @admins | @admin - Report A Message To Admins.""" """
 
 
 async def int_to_alpha(user_id: int) -> str:
