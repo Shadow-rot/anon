f28506cc878e903dd5049ce491585cf6b83379d3
Shadow-rot <looktouhid@gmail.com>
2025-03-29 17:46:12 +0530

    Update mmf.py

--- AnonXMusic/plugins/tools/mmf.py
+++ AnonXMusic/plugins/tools/mmf.py
@@ -14,7 +14,7 @@ async def mmf(_, message: Message):
         await message.reply_text("Give me text after /mmf to memify.")
         return
 
-    msg = await message.reply_text("🌬️")
+    msg = await message.reply_text("🪐")
     text = message.text.split(None, 1)[1]
     file = await app.download_media(reply_message)
 
