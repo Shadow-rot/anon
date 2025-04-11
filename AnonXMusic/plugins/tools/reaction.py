980a93ff9067c761f210d8161d2ade86551ae7bb
Shadow-rot <looktouhid@gmail.com>
2025-04-12 03:41:29 +0530

    Update reaction.py

--- AnonXMusic/plugins/tools/reaction.py
+++ AnonXMusic/plugins/tools/reaction.py
@@ -1,10 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.types.chat_reactions import AllReactions, SomeReactions
 from AnonXMusic import app
 
 
-# Emoji reactions per keyword
 EMOJI_REACTIONS = {
     "hello": "👋",
     "bye": "👍",
@@ -24,13 +22,21 @@ async def react_with_emoji(client, message: Message):
 
     for keyword, emoji in EMOJI_REACTIONS.items():
         if keyword in text:
-            # If all reactions are allowed, send any
-            if isinstance(reactions, AllReactions) or (
-                isinstance(reactions, SomeReactions) and emoji in reactions.reactions
-            ):
-                await app.send_reaction(
-                    chat_id=message.chat.id,
-                    message_id=message.id,
-                    emoji=emoji
-                )
+            # Reactions can be:
+            # - None (reactions not allowed)
+            # - list of emojis (SomeReactions)
+            # - string 'all' (some older versions)
+            # - object with .reactions attribute
+            if reactions is None:
+                break  # Reactions not allowed in this chat
+
+            if hasattr(reactions, "reactions"):  # SomeReactions
+                if emoji in reactions.reactions:
+                    await app.send_reaction(message.chat.id, message.id, emoji)
+            elif isinstance(reactions, list):  # Already a list
+                if emoji in reactions:
+                    await app.send_reaction(message.chat.id, message.id, emoji)
+            else:
+                # fallback: assume all reactions allowed
+                await app.send_reaction(message.chat.id, message.id, emoji)
             break
