22b627acc2b928cab51f3ee756ddb21988ccadaa
Shadow-rot <looktouhid@gmail.com>
2025-05-22 08:54:57 +0530

    Update anime.py

--- AnonXMusic/plugins/tools/anime.py
+++ AnonXMusic/plugins/tools/anime.py
@@ -1,12 +1,10 @@
 import os
 import aiohttp
+from io import BytesIO
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from AnonXMusic import app
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def fetch_json(url):
     async with aiohttp.ClientSession() as session:
@@ -15,17 +13,24 @@ async def fetch_json(url):
                 return await resp.json()
             return None
 
-async def fetch_image_bytes(url):
+
+async def fetch_image_bytes(image_url):
     async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
+        async with session.get(image_url) as resp:
             if resp.status == 200:
                 return await resp.read()
             return None
+
 
 async def send_anime_card(message, title, info, image_url, site_url):
     image_data = await fetch_image_bytes(image_url)
     if not image_data:
         return await message.reply("Failed to fetch image.")
+
+    image_file = BytesIO(image_data)
+    image_file.name = "anime.jpg"
+    image_file.seek(0)
+
     caption = (
         f"<b>{title}</b>\n"
         f"{info}\n\n"
@@ -34,7 +39,8 @@ async def send_anime_card(message, title, info, image_url, site_url):
     buttons = InlineKeyboardMarkup(
         [[InlineKeyboardButton("View on Website", url=site_url)]]
     )
-    await message.reply_photo(image_data, caption=caption, reply_markup=buttons)
+    await message.reply_photo(photo=image_file, caption=caption, reply_markup=buttons)
+
 
 @app.on_message(filters.command("anime"))
 async def anime_info(_, message):
@@ -53,6 +59,7 @@ async def anime_info(_, message):
     )
     await send_anime_card(message, anime['title'], info, anime['images']['jpg']['large_image_url'], anime['url'])
 
+
 @app.on_message(filters.command("character"))
 async def character_info(_, message):
     query = " ".join(message.command[1:])
@@ -64,6 +71,7 @@ async def character_info(_, message):
     char = data["data"][0]
     info = f"<b>About:</b> {char.get('about', 'No info available')[:500]}..."
     await send_anime_card(message, char['name'], info, char['images']['jpg']['image_url'], char['url'])
+
 
 @app.on_message(filters.command("manga"))
 async def manga_info(_, message):
@@ -82,6 +90,7 @@ async def manga_info(_, message):
     )
     await send_anime_card(message, manga['title'], info, manga['images']['jpg']['large_image_url'], manga['url'])
 
+
 @app.on_message(filters.command("airing"))
 async def airing_info(_, message):
     query = " ".join(message.command[1:])
@@ -98,6 +107,7 @@ async def airing_info(_, message):
     )
     await send_anime_card(message, anime['title'], info, anime['images']['jpg']['large_image_url'], anime['url'])
 
+
 @app.on_message(filters.command("upcoming"))
 async def upcoming_anime(_, message):
     data = await fetch_json("https://api.jikan.moe/v4/seasons/upcoming")
@@ -109,6 +119,7 @@ async def upcoming_anime(_, message):
     text += "\n<i>Message provided by <a href='https://t.me/siyaprobot'>Siya</a></i>"
     await message.reply(text, disable_web_page_preview=True)
 
+
 @app.on_message(filters.command("latest"))
 async def latest_anime(_, message):
     data = await fetch_json("https://api.jikan.moe/v4/seasons/now")
@@ -119,6 +130,7 @@ async def latest_anime(_, message):
         text += f"<a href='{anime['url']}'>{anime['title']}</a>\n"
     text += "\n<i>Message provided by <a href='https://t.me/siyaprobot'>Siya</a></i>"
     await message.reply(text, disable_web_page_preview=True)
+
 
 @app.on_message(filters.command("user"))
 async def user_info(_, message):
@@ -136,6 +148,7 @@ async def user_info(_, message):
     )
     await send_anime_card(message, user['username'], info, user['images']['jpg']['image_url'], user['url'])
 
+
 @app.on_message(filters.command("kaizoku"))
 async def search_kaizoku(_, message):
     query = " ".join(message.command[1:])
@@ -145,6 +158,7 @@ async def search_kaizoku(_, message):
     reply = f"<b>Results for:</b> {query}\n<a href='{search_url}'>View on AnimeKaizoku</a>\n\n<i>Message provided by <a href='https://t.me/siyaprobot'>Siya</a></i>"
     await message.reply(reply, disable_web_page_preview=True)
 
+
 @app.on_message(filters.command("kayo"))
 async def search_kayo(_, message):
     query = " ".join(message.command[1:])
@@ -153,6 +167,7 @@ async def search_kayo(_, message):
     search_url = f"https://animekayo.com/?s={query.replace(' ', '+')}"
     reply = f"<b>Results for:</b> {query}\n<a href='{search_url}'>View on AnimeKayo</a>\n\n<i>Message provided by <a href='https://t.me/siyaprobot'>Siya</a></i>"
     await message.reply(reply, disable_web_page_preview=True)
+
 
 @app.on_message(filters.command("whatanime"))
 async def what_anime(_, message):
