37e26f6f093534418d15d0f38d4f796e538f87e2
Shadow-rot <looktouhid@gmail.com>
2025-05-27 05:25:25 +0530

    Update help.py

--- AnonXMusic/utils/inline/help.py
+++ AnonXMusic/utils/inline/help.py
@@ -1,94 +1,45 @@
 from typing import Union
-
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from AnonXMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
-    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data=f"close")]
+    first = [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")]
     second = [
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
-            callback_data=f"settingsback_helper",
+            callback_data="settingsback_helper",
         ),
     ]
     mark = second if START else first
     upl = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(
-                    text=_["H_B_1"],
-                    callback_data="help_callback hb1",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_2"],
-                    callback_data="help_callback hb2",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_3"],
-                    callback_data="help_callback hb3",
-                ),
+                InlineKeyboardButton(text=_["H_B_1"], callback_data="help_callback hb1"),
+                InlineKeyboardButton(text=_["H_B_2"], callback_data="help_callback hb2"),
+                InlineKeyboardButton(text=_["H_B_3"], callback_data="help_callback hb3"),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb6",
-                ),
+                InlineKeyboardButton(text=_["H_B_4"], callback_data="help_callback hb4"),
+                InlineKeyboardButton(text=_["H_B_5"], callback_data="help_callback hb5"),
+                InlineKeyboardButton(text=_["H_B_6"], callback_data="help_callback hb6"),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_7"],
-                    callback_data="help_callback hb7",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_8"],
-                    callback_data="help_callback hb8",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_9"],
-                    callback_data="help_callback hb9",
-                ),
+                InlineKeyboardButton(text=_["H_B_7"], callback_data="help_callback hb7"),
+                InlineKeyboardButton(text=_["H_B_8"], callback_data="help_callback hb8"),
+                InlineKeyboardButton(text=_["H_B_9"], callback_data="help_callback hb9"),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_10"],
-                    callback_data="help_callback hb10",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_11"],
-                    callback_data="help_callback hb11",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_12"],
-                    callback_data="help_callback hb12",
-                ),
+                InlineKeyboardButton(text=_["H_B_10"], callback_data="help_callback hb10"),
+                InlineKeyboardButton(text=_["H_B_11"], callback_data="help_callback hb11"),
+                InlineKeyboardButton(text=_["H_B_12"], callback_data="help_callback hb12"),
             ],
             [
-                InlineKeyboardButton(
-                    text=_["H_B_13"],
-                    callback_data="help_callback hb13",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_14"],
-                    callback_data="help_callback hb14",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_15"],
-                    callback_data="help_callback hb15",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_16"],
-                    callback_data="help_callback hb16",
-                ),
+                InlineKeyboardButton(text=_["H_B_13"], callback_data="help_callback hb13"),
+                InlineKeyboardButton(text=_["H_B_14"], callback_data="help_callback hb14"),
+                InlineKeyboardButton(text=_["H_B_15"], callback_data="help_callback hb15"),
+                InlineKeyboardButton(text=_["H_B_16"], callback_data="help_callback hb16"),
             ],
             mark,
         ]
@@ -102,7 +53,7 @@ def help_back_markup(_):
             [
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"],
-                    callback_data=f"settings_back_helper",
+                    callback_data="settings_back_helper",
                 ),
             ]
         ]
@@ -120,3 +71,35 @@ def private_help_panel(_):
         ],
     ]
     return buttons
+
+
+# Handler for all 16 help buttons
+@app.on_callback_query(filters.regex(r"help_callback hb(1[0-6]|[1-9])"))
+async def help_button_handler(_, query: CallbackQuery):
+    data = query.data.split()[1]
+
+    help_texts = {
+        "hb1": "Help for Module 1",
+        "hb2": "Help for Module 2",
+        "hb3": "Help for Module 3",
+        "hb4": "Help for Module 4",
+        "hb5": "Help for Module 5",
+        "hb6": "Help for Module 6",
+        "hb7": "Help for Module 7",
+        "hb8": "Help for Module 8",
+        "hb9": "Help for Module 9",
+        "hb10": "Help for Module 10",
+        "hb11": "Help for Module 11",
+        "hb12": "Help for Module 12",
+        "hb13": "Help for Module 13",
+        "hb14": "Help for Module 14",
+        "hb15": "Help for Module 15",
+        "hb16": "Help for Module 16",
+    }
+
+    help_text = help_texts.get(data, "Help not found.")
+
+    await query.message.edit_text(
+        text=help_text,
+        reply_markup=help_back_markup({"BACK_BUTTON": "Back"})
+    )
