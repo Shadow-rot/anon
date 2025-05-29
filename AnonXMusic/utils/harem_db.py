7ba1f7c2ddf130f87fd97c04844b646ff998f017
Shadow-rot <looktouhid@gmail.com>
2025-05-30 02:31:00 +0530

    Create harem_db.py

---
+++ AnonXMusic/utils/harem_db.py
@@ -0,0 +1,12 @@
+# harem_db.py
+from motor.motor_asyncio import AsyncIOMotorClient
+
+HAREM_URI = "mongodb+srv://Sha:u8KqYML48zhyeWB@cluster0.ebq5nwm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+harem_client = AsyncIOMotorClient(HAREM_URI)
+
+# Use a separate database like 'harem_data' (not affecting your main one)
+harem_db = harem_client["harem_data"]
+
+# Collections for characters and users
+harem_collection = harem_db["characters"]
+harem_users = harem_db["users"]
