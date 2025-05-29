8a13929cb4652ba4c0a1e56819b3154e7b5fd127
Shadow-rot <looktouhid@gmail.com>
2025-05-30 03:18:39 +0530

    Update harem_db.py

--- AnonXMusic/utils/harem_db.py
+++ AnonXMusic/utils/harem_db.py
@@ -1,13 +1,14 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
+# MongoDB connection URI
 HAREM_URI = "mongodb+srv://Sha:u8KqYML48zhyeWB@cluster0.ebq5nwm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Connect to MongoDB using Motor
 harem_client = AsyncIOMotorClient(HAREM_URI)
 
-Use a separate database like 'harem_data' (not affecting your main one)
-
+# Use a separate database (not affecting your main one)
 harem_db = harem_client["harem_data"]
 
-Collections for characters and users
-
+# Collections for characters and users
 harem_collection = harem_db["characters"]
 harem_users = harem_db["users"]
