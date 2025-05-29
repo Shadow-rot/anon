592c76d2e709f6d6516ef9e727ca1bab32f604a4
Shadow-rot <looktouhid@gmail.com>
2025-05-30 03:17:19 +0530

    Update harem_db.py

--- AnonXMusic/utils/harem_db.py
+++ AnonXMusic/utils/harem_db.py
@@ -1,19 +1,13 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 
-# Mongo URI for Harem system
 HAREM_URI = "mongodb+srv://Sha:u8KqYML48zhyeWB@cluster0.ebq5nwm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-# Initialize MongoDB client
 harem_client = AsyncIOMotorClient(HAREM_URI)
 
-# Database for harem/shop system
+Use a separate database like 'harem_data' (not affecting your main one)
+
 harem_db = harem_client["harem_data"]
 
-# Collections
-collection = harem_db["characters"]     # Stores waifu/character data
-harem_users = harem_db["users"]         # Stores user economy, xp, harem, etc.
-db = harem_db                           # For any extra collections like sequences
+Collections for characters and users
 
-# Optional: create sequence counter collection if not exists
-# This is used for auto-incrementing character IDs
-sequences = harem_db["sequences"]
+harem_collection = harem_db["characters"]
+harem_users = harem_db["users"]
