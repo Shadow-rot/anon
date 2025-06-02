2f6b74fb330b965aa315892b82a1915264af8a5f
Shadow-rot <looktouhid@gmail.com>
2025-06-02 07:53:46 +0530

    Update inlinequery.py

--- AnonXMusic/plugins/grabber/inlinequery.py
+++ AnonXMusic/plugins/grabber/inlinequery.py
@@ -22,29 +22,32 @@ async def inlinequery(client: Client, query: types.InlineQuery):
     offset = int(query.offset) if query.offset else 0
     all_characters = []
 
+    # User collection mode
     if query.query.startswith('collection.'):
         parts = query.query.split(' ')
-        user_id = parts[0].split('.')[1]
+        first = parts[0].split('.')
+        if len(first) < 2 or not first[1].isdigit():
+            return  # malformed query
+
+        user_id = first[1]
         search_terms = ' '.join(parts[1:])
 
-        if user_id.isdigit():
-            if user_id in user_collection_cache:
-                user = user_collection_cache[user_id]
-            else:
-                user = await users_col.find_one({'id': int(user_id)})
-                user_collection_cache[user_id] = user
-
-            if user:
-                all_characters = list({v['id']: v for v in user['characters']}.values())
-                if search_terms:
-                    regex = re.compile(search_terms, re.IGNORECASE)
-                    all_characters = [
-                        c for c in all_characters
-                        if regex.search(c['name']) or regex.search(c['anime'])
-                    ]
+        if user_id in user_collection_cache:
+            user = user_collection_cache[user_id]
         else:
-            all_characters = []
+            user = await users_col.find_one({'id': int(user_id)})
+            user_collection_cache[user_id] = user
+
+        if user:
+            all_characters = list({v['id']: v for v in user['characters']}.values())
+            if search_terms:
+                regex = re.compile(search_terms, re.IGNORECASE)
+                all_characters = [
+                    c for c in all_characters
+                    if regex.search(c['name']) or regex.search(c['anime'])
+                ]
     else:
+        # Global search mode
         if query.query:
             regex = re.compile(query.query, re.IGNORECASE)
             all_characters = await collection.find({
@@ -60,6 +63,7 @@ async def inlinequery(client: Client, query: types.InlineQuery):
                 all_characters = await collection.find({}).to_list(length=None)
                 all_characters_cache['all_characters'] = all_characters
 
+    # Paginate
     characters = all_characters[offset:offset + 50]
     next_offset = str(offset + len(characters)) if len(characters) == 50 else ""
 
@@ -69,7 +73,6 @@ async def inlinequery(client: Client, query: types.InlineQuery):
         anime_characters = await collection.count_documents({'anime': character['anime']})
         total_characters = len(all_characters)
 
-        title = str(total_characters)
         if query.query.startswith('collection.'):
             user_character_count = sum(c['id'] == character['id'] for c in user['characters'])
             user_anime_characters = sum(c['anime'] == character['anime'] for c in user['characters'])
@@ -92,17 +95,26 @@ async def inlinequery(client: Client, query: types.InlineQuery):
 
         results.append(
             types.InlineQueryResultPhoto(
-                title=title,
+                title=character['name'],
                 thumb_url=character['img_url'],
                 photo_url=character['img_url'],
                 caption=caption
             )
         )
 
-    await client.answer_inline_query(
-        query.id,
-        results=results,
-        next_offset=next_offset,
-        cache_time=5,
-        is_gallery=True
-    )
+    # Only answer if results exist
+    if results:
+        await client.answer_inline_query(
+            query.id,
+            results=results,
+            next_offset=next_offset,
+            cache_time=5,
+            is_gallery=True
+        )
+    else:
+        await client.answer_inline_query(
+            query.id,
+            results=[],
+            switch_pm_text="No results found!",
+            switch_pm_parameter="start"
+        )
