833637df7fe1ab0fab81c922af459651e8a0894d
Shadow-rot <looktouhid@gmail.com>
2025-06-07 04:43:20 +0530

    Update userid.py

--- AnonXMusic/plugins/tools/userid.py
+++ AnonXMusic/plugins/tools/userid.py
@@ -1,55 +1,82 @@
+from datetime import datetime
+from pyrogram import filters
+from pyrogram.enums import ParseMode, ChatType
+from pyrogram.types import Message
 from AnonXMusic import app
-from pyrogram import Client, filters
 
-from pyrogram.enums import ParseMode
-
-####
-
-@app.on_message(filters.command('id'))
-async def getid(client, message):
-    chat = message.chat
-    your_id = message.from_user.id
-    message_id = message.id
+@app.on_message(filters.command("id"))
+async def get_ids(client, message: Message):
     reply = message.reply_to_message
+    text = ""
 
-    text = f"**[ᴍᴇssᴀɢᴇ ɪᴅ:]({message.link})** `{message_id}`\n"
-    text += f"**[ʏᴏᴜʀ ɪᴅ:](tg://user?id={your_id})** `{your_id}`\n"
+    chat = message.chat
+    from_user = message.from_user
 
-    if not message.command:
-        message.command = message.text.split()
+    # Message and user info
+    text += f"Message ID: {message.id}\n"
+    if from_user:
+        text += f"Your ID: {from_user.id} (tg://user?id={from_user.id})\n"
+        text += f"Is Bot: {from_user.is_bot}\n"
 
-    if not message.command:
-        message.command = message.text.split()
+    # Chat Info
+    if chat.type in [ChatType.GROUP, ChatType.SUPERGROUP, ChatType.CHANNEL]:
+        chat_link = f"https://t.me/{chat.username}" if chat.username else "Private"
+        text += f"Chat ID: {chat.id}\n"
+        text += f"Chat Title: {chat.title}\n"
+        text += f"Chat Link: {chat_link}\n"
 
-    if len(message.command) == 2:
+    elif chat.type == ChatType.PRIVATE:
+        text += f"Private Chat ID: {chat.id}\n"
+
+    # Replied Message Info
+    if reply:
+        if reply.from_user:
+            user = reply.from_user
+            text += "\nReplied Message:\n"
+            text += f"Name: {user.first_name}\n"
+            text += f"ID: {user.id}\n"
+            if user.username:
+                text += f"Username: @{user.username}\n"
+            text += f"Bot: {user.is_bot}\n"
+            text += f"Replied Message ID: {reply.id}\n"
+
+        if reply.forward_from:
+            fwd_user = reply.forward_from
+            text += "\nForwarded From User:\n"
+            text += f"Name: {fwd_user.first_name}\n"
+            text += f"ID: {fwd_user.id}\n"
+            if fwd_user.username:
+                text += f"Username: @{fwd_user.username}\n"
+
+        elif reply.forward_from_chat:
+            fwd_chat = reply.forward_from_chat
+            text += "\nForwarded From Channel/Group:\n"
+            text += f"Title: {fwd_chat.title}\n"
+            text += f"ID: {fwd_chat.id}\n"
+            if fwd_chat.username:
+                text += f"Username: @{fwd_chat.username}\n"
+
+        if reply.sender_chat:
+            sender = reply.sender_chat
+            text += "\nSender Chat (Anonymous Admin / Channel):\n"
+            text += f"Title: {sender.title}\n"
+            text += f"ID: {sender.id}\n"
+
+    # Optional: Check target from command
+    if len(message.command) > 1:
+        target = message.text.split(None, 1)[1]
         try:
-            split = message.text.split(None, 1)[1].strip()
-            user_id = (await client.get_users(split)).id
-            text += f"**[ᴜsᴇʀ ɪᴅ:](tg://user?id={user_id})** `{user_id}`\n"
-
+            user = await client.get_users(target)
+            text += "\nTarget User from Command:\n"
+            text += f"Name: {user.first_name}\n"
+            text += f"ID: {user.id}\n"
+            if user.username:
+                text += f"Username: @{user.username}\n"
+            text += f"Bot: {user.is_bot}\n"
         except Exception:
-            return await message.reply_text("ᴛʜɪs ᴜsᴇʀ ᴅᴏᴇsɴ'ᴛ ᴇxɪsᴛ.", quote=True)
+            text += f"\nFailed to fetch user '{target}'."
 
-    text += f"**[ᴄʜᴀᴛ ɪᴅ:](https://t.me/{chat.username})** `{chat.id}`\n\n"
+    # Timestamp
+    text += f"\nTimestamp: {datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')} UTC\n"
 
-    if (
-        not getattr(reply, "empty", True)
-        and not message.forward_from_chat
-        and not reply.sender_chat
-    ):
-        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴍᴇssᴀɢᴇ ɪᴅ:]({reply.link})** `{reply.id}`\n"
-        text += f"**[ʀᴇᴘʟɪᴇᴅ ᴜsᴇʀ ɪᴅ:](tg://user?id={reply.from_user.id})** `{reply.from_user.id}`\n\n"
-
-    if reply and reply.forward_from_chat:
-        text += f"ᴛʜᴇ ғᴏʀᴡᴀʀᴅᴇᴅ ᴄʜᴀɴɴᴇʟ, {reply.forward_from_chat.title}, ʜᴀs ᴀɴ ɪᴅ ᴏғ `{reply.forward_from_chat.id}`\n\n"
-        print(reply.forward_from_chat)
-
-    if reply and reply.sender_chat:
-        text += f"ɪᴅ ᴏғ ᴛʜᴇ ʀᴇᴘʟɪᴇᴅ ᴄʜᴀᴛ/ᴄʜᴀɴɴᴇʟ, ɪs `{reply.sender_chat.id}`"
-        print(reply.sender_chat)
-
-    await message.reply_text(
-        text,
-        disable_web_page_preview=True,
-        parse_mode=ParseMode.DEFAULT,
-    )
+    await message.reply_text(text, parse_mode=ParseMode.DEFAULT, disable_web_page_preview=True)
