c6b7bc3a1c7c4f97b66f676ae550ed1939a02edc
Shadow-rot <looktouhid@gmail.com>
2025-06-09 16:58:48 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -27,9 +27,9 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_5"], url=config.ANIMES),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")
+        ],
     ]
     return buttons
