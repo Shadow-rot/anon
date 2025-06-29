bff65053fdfae9a3c37545c7069a7cba135a35cc
Shadow-rot <looktouhid@gmail.com>
2025-06-30 05:25:10 +0530

    Update autoend.py

--- AnonXMusic/plugins/sudo/autoend.py
+++ AnonXMusic/plugins/sudo/autoend.py
@@ -2,11 +2,13 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
 from AnonXMusic.utils.database import autoend_off, autoend_on
+from config import OWNER_ID
 
 
-@app.on_message(filters.command("autoend") & SUDOERS)
+only_owner = filters.user(OWNER_ID)
+
+@app.on_message(filters.command("autoend") & only_owner)
 async def auto_end_stream(_, message: Message):
     usage = "<b>ᴇxᴀᴍᴘʟᴇ :</b>\n\n/autoend [ᴇɴᴀʙʟᴇ | ᴅɪsᴀʙʟᴇ]"
     if len(message.command) != 2:
